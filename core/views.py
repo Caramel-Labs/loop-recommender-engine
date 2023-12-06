8dc9c0af0ebee392dd53e6a0172c9813b68389dc
Ravindu Aratchige <109760834+ravi-aratchige@users.noreply.github.com>
2023-12-06 17:03:14 +0530

    UPDATE: changed folder structure

--- core/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-
-def index(request):
-    print("index() function of core app fired")
-    return HttpResponse(request, "Hello world from the core app!")
--- engine/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("like/", views.handle_like, name="handle_like"),
-    path("bookmark/", views.handle_bookmark, name="handle_bookmark"),
-]
